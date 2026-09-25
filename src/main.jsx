70bbb44db545b38b522e807910200ffad0f98e8d
GuetaMac <aletamackenny@gmail.com>
2026-09-25 14:03:21 +0800

    ato update in the app whenever there's an update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { registerSW } from "virtual:pwa-register";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+registerSW({
+  immediate: true,
+  onRegisteredSW(swUrl, registration) {
+    // check for updates every time the app comes back to foreground
+    document.addEventListener("visibilitychange", () => {
+      if (document.visibilityState === "visible") {
+        registration?.update();
+      }
+    });
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
-)
+);
